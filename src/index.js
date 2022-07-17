89e8958e3653443ad29da71d4aba6f3408676d9f
Mateusz Wiczuk <mwiczuk99@gmail.com>
2022-07-17 23:27:25 +0200

    Fix whitespaces

--- src/index.js
+++ src/index.js
@@ -31,13 +31,13 @@ client.on('interactionCreate', async interaction => {
 
     const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+    if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+    console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
 
